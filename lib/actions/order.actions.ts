bd9fb3fb6427619412a28f9c4f329bcef180de26
Rugved Kulkarni <rugvedrk1189@gmail.com>
2024-05-28 23:56:27 +0530

    -changed the currency to fix issues with clerk's indian location

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -22,11 +22,12 @@ export const checkoutOrder = async (order: CheckoutOrderParams) => {
       line_items: [
         {
           price_data: {
-            currency: 'usd',
+            currency: 'inr',
             unit_amount: price,
             product_data: {
               name: order.eventTitle
             }
+            
           },
           quantity: 1
         },
