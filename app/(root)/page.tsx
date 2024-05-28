fce24c91e9f74697d68e91bfb665227e9b5ca4a8
Rugved Kulkarni <rugvedrk1189@gmail.com>
2024-05-28 18:09:48 +0530

    nm

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -25,8 +25,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
             {/* <h1 className="h1-bold">ProPass: Elevate Every Occasion</h1> */}
-            <h1 className="h1-bold"><div className="text-crimson inline">Pro</div><div className="text-blue-500 inline">Pass</div>: 
-            Elevate Every Occasion</h1>
+            <h1 className="h1-bold">ProPass: Elevate Every Occasion</h1>
             <p className="p-regular-20 md:p-regular-24">Create, connect, and celebrate with unrivaled flair and functionality.</p>
             <Button size="lg" asChild className="button w-full sm:w-fit">
               <Link href="#events">
